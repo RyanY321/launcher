9bb8867e85e9f714d1a78a8fd40025b535e4e18e
Ryan <ryanyoungjohn321@gmail.com>
2023-01-11 20:42:40 -0500

    auto update

--- launcher.bat
+++ launcher.bat
@@ -1,5 +1,12 @@
 @echo off
 chcp 65001 >nul
 
+cd C:\
+rmdir /S /Q C:\CobraClient
+mkdir CobraClient
+attrib +H C:\CobraClient
+cd C:\CobraClient
+git clone https://github.com/RyanY321/CobraClientInstall.git
+
 cd C:\CobraClient\CobraClientInstall
 call C:\CobraClient\CobraClientInstall\CobraClient.bat
